45f1618121b2b329db8595e6d761a3866a9fd660
tanverrana <tanversobur@gmail.com>
2022-07-18 13:08:36 +0600

    condition statement

--- guessing-game.c
+++ guessing-game.c
@@ -5,6 +5,25 @@ int main()
 {
     srand(time(0));
     int hidden = rand() % 100;
-    printf("%d\n", hidden);
+    //printf("%d\n", hidden);
+    int numer_of_guesses = 0;
+    while (1)
+    {
+        int guess;
+        scanf("%d", &guess);
+        if (guess == hidden)
+        {
+            printf("You are right\n");
+            break;
+        }
+        else if (guess > hidden)
+        {
+            printf("Guess Smaller Value\n");
+        }
+        else
+        {
+            printf("Guess larger value\n");
+        }
+    }
     return 0;
 }
